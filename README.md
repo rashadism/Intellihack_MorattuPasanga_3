7453865530d1144b88ef5136d15153e169d31f1c
Rashad M <rashad.20@cse.mrt.ac.lk>
2024-05-04 16:25:17 +0530

    Added data retrieval

---
+++ README.md
@@ -0,0 +1,5 @@
+How to get this thing running?
+
+1. Fill in the values in .env.example and save it as .env in the root folder
+2. run `pip install -r requirements.txt`
+3. run `streamlit run app.py`
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import streamlit as st
 
 from langchain_groq import ChatGroq
@@ -12,17 +13,50 @@ from langchain_community.chat_message_histories import (
     StreamlitChatMessageHistory,
 )
 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+
+
 from langchain.memory import ChatMessageHistory
 
 import streamlit as st
 import os
 
+
+
 load_dotenv()
 GROQ_API_KEY=os.environ['GROQ_API_KEY']
 
+loader=PyPDFLoader('data/loan_data.pdf')
+docs=loader.load()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+formatted_docs=format_docs(docs)
+
+# text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+# documents=text_splitter.split_documents(docs)
+
+
+
+system_message=f"""
+You are a chatbot assists customers of SmartBank with inquiries about their eligibility for loans 
+and to provide information about the loan application process. You are to provide the following functionality.
+
+Loan Eligibility Check: The system should allow users to check their eligibility for different types of loans  based on various criteria such as credit score, income level, employment status, and existing debts. 
+Loan Products Information: Users should be able to obtain information about the different types of  loans offered by the institution, including their features, interest rates, repayment terms, and eligibility  requirements. 
+Application Process Guidance: The system should guide users through the loan application process,  providing information about the documents required, the steps involved, and the timeline for approval  and disbursement. 
+FAQs and Troubleshooting: The system should provide answers to frequently asked questions about  loans, such as how to improve credit score, what to do if an application is rejected, and how to calculate  loan repayments. 
+Personalized Recommendations: Based on the user's financial situation, the system should offer  personalized recommendations for suitable loan products and tips for improving eligibility.
+
+You are allowed to use the following information as context to answer user queries.
+\n\n{formatted_docs}"""
+
+print(system_message)
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are an assistant who gives information on bank loans. Please answer the questions"),
+        ("system",system_message),
         MessagesPlaceholder(variable_name="history"),
         ("human", "{question}")
     ]
