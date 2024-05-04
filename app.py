a294db6f5ebd925c4e58e15dfd76926e87d9df04
Rashad M <rashad.20@cse.mrt.ac.lk>
2024-05-04 14:40:21 +0530

    UI done

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ from dotenv import load_dotenv
 import streamlit as st
 
 from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from langchain_community.chat_message_histories import (
+    StreamlitChatMessageHistory,
+)
+
+from langchain.memory import ChatMessageHistory
 
 import streamlit as st
 import os
@@ -13,4 +20,39 @@ import os
 load_dotenv()
 GROQ_API_KEY=os.environ['GROQ_API_KEY']
 
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are an assistant who gives information on bank loans. Please answer the questions"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}")
+    ]
+)
+
 llm=ChatGroq(model="llama3-70b-8192")
+
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+chat_history = ChatMessageHistory()
+
+msgs=StreamlitChatMessageHistory(key="chat_messages")
+
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("Hi I am bank assistant. How can I help you?")
+
+history_chain = RunnableWithMessageHistory(
+  chain,
+  lambda session_id: msgs,
+  input_messages_key="question",
+  history_messages_key="history"
+)
+
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+if prompt := st.chat_input():
+    st.chat_message("human").write(prompt)
+
+    config = {"configurable": {"session_id": "any"}}
+    response = history_chain.invoke({"question": prompt}, config)
+    st.chat_message("ai").write(response)
